fbecd36bcb0fc658a23823835923d39446b26eb1
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2023-12-04 14:34:40 +0100

    Added solution to Day 2 Part 2.

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -60,10 +60,68 @@ fn part_1(input: &str) -> u32 {
         .sum()
 }
 
+fn part_2(input: &str) -> u32 {
+    input
+        .split('\n')
+        .filter(|line| !line.is_empty())
+        .filter_map(|line| line.split_once(':').map(|(_, game_value)| game_value))
+        .map(|game_values| {
+            game_values
+                .split(';')
+                .map(|round| {
+                    round
+                        .split(", ")
+                        .filter_map(|cube_values| {
+                            cube_values
+                                .trim()
+                                .split_once(' ')
+                                .and_then(|(count, color)| {
+                                    if let Ok(count) = count.parse::<u32>() {
+                                        Some((count, color))
+                                    } else {
+                                        None
+                                    }
+                                })
+                                .and_then(|(count, color)| match color {
+                                    "red" => Some((count, Color::Red)),
+                                    "green" => Some((count, Color::Green)),
+                                    "blue" => Some((count, Color::Blue)),
+                                    _ => None,
+                                })
+                        })
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>()
+        })
+        .map(|rounds| {
+            let mut min_red = 0;
+            let mut min_green = 0;
+            let mut min_blue = 0;
+
+            for round in rounds {
+                for (value, color) in round {
+                    match color {
+                        Color::Red if value > min_red => min_red = value,
+                        Color::Green if value > min_green => min_green = value,
+                        Color::Blue if value > min_blue => min_blue = value,
+                        _ => (),
+                    }
+                }
+            }
+
+            (min_red, min_green, min_blue)
+        })
+        .map(|(red, green, blue)| red * green * blue)
+        .sum()
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let path = &args[1];
     let input = std::fs::read_to_string(path).unwrap();
     let part_1_result = part_1(&input);
     println!("Day 2 Part 1: {}", part_1_result);
+
+    let part_2_result = part_2(&input);
+    println!("Day 2 Part 2: {}", part_2_result);
 }
